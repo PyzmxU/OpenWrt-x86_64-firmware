b3fecc0e5b192d93b141e0cdb41c3441345028c7
PyzmxU <36581905+PyzmxU@users.noreply.github.com>
2021-06-26 21:31:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,8 +14,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-
-#sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default   #软件很多
+#sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default   #软件很多
 mkdir -p files/etc/config
 wget https://raw.githubusercontent.com/PyzmxU/OpenWrt-x86_64-firmware/main/firewall -O files/etc/config/firewall
 wget https://raw.githubusercontent.com/PyzmxU/OpenWrt-x86_64-firmware/main/network -O files/etc/config/network
