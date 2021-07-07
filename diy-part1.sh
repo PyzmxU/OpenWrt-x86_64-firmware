5930a554a4b51707ba19b63647ae1c03e5dfcb9c
PyzmxU <36581905+PyzmxU@users.noreply.github.com>
2021-07-07 21:53:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,3 +1,3 @@
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default
 #sed -i '$a src-git lua-maxminddb https://github.com/jerrykuku/lua-maxminddb.git' feeds.conf.default
-sed -i '$a src-git luci-app-vssr https://github.com/jerrykuku/luci-app-vssrl.git' feeds.conf.default
+#sed -i '$a src-git luci-app-vssr https://github.com/jerrykuku/luci-app-vssrl.git' feeds.conf.default
