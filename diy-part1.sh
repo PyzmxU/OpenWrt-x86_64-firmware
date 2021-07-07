6996f731f297fc361ebd1c74086dc2a9a439fbac
PyzmxU <36581905+PyzmxU@users.noreply.github.com>
2021-07-07 21:13:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,9 +15,10 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default #软件源
-#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default   #软件源
-#sed -i '$a src-git lua-maxminddb https://github.com/jerrykuku/lua-maxminddb.git' feeds.conf.default   #软件源
-sed -i '$a src-git jerrykuku https://github.com/jerrykuku/luci-app-vssr' feeds.conf.default   
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default  
+sed -i '$a src-git lua-maxminddb https://github.com/jerrykuku/lua-maxminddb' feeds.conf.default   
+sed -i '$a src-git uci-app-vssr https://github.com/jerrykuku/luci-app-vssr' feeds.conf.default   
+
 #sed -i '$a src-git luci-theme-argon https://github.com/jerrykuku/luci-theme-argon.git' feeds.conf.default   #软件源
 
 
